4cb5720453a821325c32950e841f2ee500b02618
Luis Valenzuela <lvalenzuela@longbourn.cl>
2014-11-25 10:26:00 -0300

    asdf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #precompile vendor assets
+  config.assets.precompile = [ Proc.new { |path, fn| fn =~ /app\/assets/ && !%w(.js .css).include?(File.extname(path)) },
+                             /(?:\/|\\|\A)application\.(css|js)$/ ]
 end
